9a800f3ab245241518cfdccf58102ed3b8a1dfc5
Yehyeok Bang <qkddpgur318@gmail.com>
2024-11-07 20:06:14 +0900

    feat: 현재 보유 상품 안내 메시지 출력 기능 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -7,5 +7,6 @@ public class Application {
     public static void main(String[] args) {
         OutputView outputView = new OutputView();
         outputView.printWelcomeMessage();
+        outputView.printCurrentItemsMessage();
     }
 }
